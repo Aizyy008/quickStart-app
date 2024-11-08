078fd6a72304436cc1b55f783de15ac773181b12
Aizyy008 <malikaizan198@gmail.com>
2024-11-08 18:23:05 +0500

    updated quickstep

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,20 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:quickstep_app/auth_wrapper.dart';
+import 'package:quickstep_app/screens/movements/map/movement_live_map.dart';
 import 'package:quickstep_app/services/hive_service.dart';
 import 'package:quickstep_app/utils/colors.dart';
 import 'package:quickstep_app/utils/theme.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+import 'controllers/movements_controller.dart';
+import 'models/movement.dart';
 import 'screens/authentication/welcome.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // Initialize MovementController using Get.put()
+  Get.put(MovementController());
   //Running flutter application
   runApp(const AppWidget());
 }
@@ -33,7 +39,7 @@ class AppWidget extends StatelessWidget {
           home: child,
         );
       },
-      child: const WelcomeScreen(),
+      child: WelcomeScreen(),
     );
   }
 }
